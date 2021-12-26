3f1924c176fe5a9b3bb645d44c97e3fd6bc79b26
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2021-12-26 21:05:32 +0100

    Update is_unique.java

--- Arrays_and_Strings/is_unique.java
+++ Arrays_and_Strings/is_unique.java
@@ -1,16 +1,14 @@
-import java.util.HashSet;
-
-public class Solution {
-	public boolean is_unique(String s) {
-	HashSet<Character> set  = new HashSet<>();
-	for (int i = 0; i < s.length(); i++) {
-		if (!set.contains(s.charAt(i))) {
-			set.add(s.charAt(i));
-		} else {
-			return false;
+/**
+ * implement an algorithm to determine if a string has all unique characters
+ */
+public static boolean is_unique(String s) {
+		HashSet<Character> set  = new HashSet<>();
+		for (int i = 0; i < s.length(); i++) {
+				if (!set.contains(s.charAt(i))) {
+						set.add(s.charAt(i));
+				} else {
+						return false;
+				}
 		}
-	}
-	return true;
-	}
+		return true;
 }
-	 
